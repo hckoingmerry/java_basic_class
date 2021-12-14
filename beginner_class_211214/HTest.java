5dd4027a8610157e25320c63248771bc9e074067
hckoingmerry <95738315+hckoingmerry@users.noreply.github.com>
2021-12-14 16:43:58 +0900

    fix

--- beginner_class_211214/HTest.java
+++ beginner_class_211214/HTest.java
@@ -1,17 +1,18 @@
 public class HTest {
-    public void sayNick(String nick) {
-        try {
+    public void sayNick(String nick) throws HFoolException {
             if("fool".equals(nick)) {
                 throw new HFoolException();
             } System.out.println("당신의 별명은 " + nick + "입니다.");
-        } catch(HFoolException e) {
-            System.out.println("에러가 발생했습니다.");
-        }
     }
     
     public static void main(String[] args) {
         HTest test = new HTest();
-        test.sayNick("fool");
-        test.sayNick("genious");
+        try {
+            test.sayNick("fool");
+            test.sayNick("genious");
+        } catch(HFoolException e) {
+            System.out.println("에러가 발생했습니다.");
+        }
+        
     }
 }
