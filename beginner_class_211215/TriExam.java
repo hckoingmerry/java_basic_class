d480bdc19e8ebc29aa129d7b0acbab4a4a9c22d2
hckoingmerry <95738315+hckoingmerry@users.noreply.github.com>
2021-12-15 17:29:05 +0900

    save

---
+++ beginner_class_211215/TriExam.java
@@ -0,0 +1,16 @@
+public class TriExam {
+    public static void main(String[] args) {
+        
+        /*
+        삼항연산자(조건)
+        - 만족하면 true, 아니면 false
+        - 첫번째 피연산자 조건식의 평가 결과에 따라 true면 두번째 항, false면 세번째 항의 값을 반환한다. (조건식의 괄호는 필수 아님)
+
+        if (조건식) {
+            true;
+        } else {
+            false;
+        }
+        */
+    }
+}
